15fb0f5f3a5310b8f07df15ec96d6ba272cc9656
user <kxchen@seu.edu.cn>
2025-09-21 22:27:02 +0800

    lora

--- src/algos/3_IVLA/trainer.py
+++ src/algos/3_IVLA/trainer.py
@@ -31,3 +31,4 @@ class Trainer(BaseTrainer):
 
 
 
+
--- src/algos/4_LoRA/run.sh
+++ src/algos/4_LoRA/run.sh
@@ -1,8 +1,8 @@
 python3 -u main.py  -data DomainNet -hd sketch -sd quickdraw    --log_name  lora --num_shots 2
-python3 -u main.py  -data DomainNet -hd quickdraw -sd sketch    --log_name  lora --num_shots 2
-python3 -u main.py  -data DomainNet -hd painting -sd infograph  --log_name  lora --num_shots 2
-python3 -u main.py  -data DomainNet -hd infograph -sd painting  --log_name  lora --num_shots 2
-python3 -u main.py  -data DomainNet -hd clipart -sd painting    --log_name  lora --num_shots 2
-
-python3 -u main.py  -e 20 -data Sketchy  -bs 24 --log_name ivla --num_shots 2
-python3 -u main.py  -e 20 -data TUBerlin -bs 24 --log_name ivla --num_shots 2
+#python3 -u main.py  -data DomainNet -hd quickdraw -sd sketch    --log_name  lora --num_shots 2
+#python3 -u main.py  -data DomainNet -hd painting -sd infograph  --log_name  lora --num_shots 2
+#python3 -u main.py  -data DomainNet -hd infograph -sd painting  --log_name  lora --num_shots 2
+#python3 -u main.py  -data DomainNet -hd clipart -sd painting    --log_name  lora --num_shots 2
+#
+#python3 -u main.py  -e 20 -data Sketchy  -bs 24 --log_name ivla --num_shots 2
+#python3 -u main.py  -e 20 -data TUBerlin -bs 24 --log_name ivla --num_shots 2
